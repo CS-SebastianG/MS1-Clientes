88f7b2a196061bcbd0ace2ef268c59330a3b1d41
CS-SebastianG <sebastian.gallegos.m@utec.edu.pe>
2026-09-20 17:12:12 -0500

    Cambiando el Puerto

--- app/database.py
+++ app/database.py
@@ -5,7 +5,7 @@ import time
 
 DB_CONFIG = {
     "host": os.getenv("DB_HOST", "127.0.0.1"),
-    "port": int(os.getenv("DB_PORT", 3307)),
+    "port": int(os.getenv("DB_PORT", 3306)),
     "user": os.getenv("DB_USER", "root"),
     "password": os.getenv("DB_PASSWORD", "rootpassword"),
     "database": os.getenv("DB_NAME", "restaurante_db"),
